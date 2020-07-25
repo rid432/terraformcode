c4ff5b5d8a3ee2066586a42a50e042c18de2b9f3
rid432 <67203514+rid432@users.noreply.github.com>
2020-07-25 12:06:30 -0700

    VPC command to implement vpc

---
+++ vpc.tf
@@ -0,0 +1,24 @@
+resource "aws_vpc" "Customer" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Terra_Customer_VPC"
+  }
+}
+  resource "aws_subnet" "Privatecustomer" {
+  vpc_id     = "vpc-0648f2628658281e5"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Terra_Private_Customer_Subnet"
+  }
+  }
+   resource "aws_subnet" "Publiccustomer" {
+  vpc_id     = "vpc-0648f2628658281e5"
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "Terra_Public_Customer_Subnet"
+  }
+}
